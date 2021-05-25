27d5f039d70a3da3d3e22189d531814303bd7277
vaniameith <vameitaqiyyah@gmail.com>
2021-05-25 17:30:59 +0700

    coba.blade.php

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,5 +11,29 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// mix.react('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css');
+
+// mix.webpackConfig({
+//     resolve: {
+//         modules: [
+//             path.resolve(__dirname, 'vendor/laravel/spark/resources/assets/js')
+//         ]
+//     }
+// });
+
+// mix.styles([ 
+//     'public/inspinia/css/bootstrap.min.css',
+//     'public/inspinia/css/animate.css',
+// ],'public/css/template.css').version();
+
+// mix.scripts([
+//     'public/inspinia/js/jquery-3.1.1.min.js', 
+//     'public/inspinia/js/bootstrap.js',
+//     'public/inspinia/js/popper.min.js',
+//     'public/inspinia/js/plugins/metisMenu/jquery.metisMenu.js',
+//     'public/inspinia/js/plugins/slimscroll/jquery.slimscroll.min.js',
+//     'public/inspinia/js/inspinia.js',
+//     'public/inspinia/js/plugins/pace/pace.min.js'  
+// ], 'public/js/template.js').version();
+
