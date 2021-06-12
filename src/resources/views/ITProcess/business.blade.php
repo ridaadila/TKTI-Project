c136f0ae45facaddb7b260825e1d1069ecfff849
vaniameith <vameitaqiyyah@gmail.com>
2021-06-12 15:37:08 +0700

    Update business.blade.php

--- src/resources/views/ITProcess/business.blade.php
+++ src/resources/views/ITProcess/business.blade.php
@@ -10,12 +10,13 @@
 </div>
 
 <main>
+    <!-- tinggal diulang pakai foreach -->
     <div class="mx-5 mt-5">
         <div class="p-3 mb-4 rounded-3" style="background-color: #e4efe7;">
             <div class="container-fluid py-5">
                 <div class="row">
                     <div class="col-3">
-                        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+                        <h1 class="display-5 fw-bold"> [nama bagian bussiness goals] </h1>
                     </div>
                     <div class="col-9">
                         <div class="row me-5">
@@ -31,72 +32,7 @@
             </div>
         </div>
     </div>
-
-    <div class="mx-5">
-        <div class="p-3 mb-4 rounded-3" style="background-color: #e4efe7;">
-            <div class="container-fluid py-5">
-                <div class="row">
-                    <div class="col-3">
-                        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-                    </div>
-                    <div class="col-9">
-                        <div class="row me-5">
-                            <div class="col-10">yayaya</div>
-                            <hr>
-                            <div class="col-10">yayaya</div>
-                            <hr>
-                            <div class="col-10">yayaya</div>
-                            <hr>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="mx-5">
-        <div class="p-3 mb-4 rounded-3" style="background-color: #e4efe7;">
-            <div class="container-fluid py-5">
-                <div class="row">
-                    <div class="col-3">
-                        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-                    </div>
-                    <div class="col-9">
-                        <div class="row me-5">
-                            <div class="col-10">yayaya</div>
-                            <hr>
-                            <div class="col-10">yayaya</div>
-                            <hr>
-                            <div class="col-10">yayaya</div>
-                            <hr>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="mx-5">
-        <div class="p-3 mb-4 rounded-3" style="background-color: #e4efe7;">
-            <div class="container-fluid py-5">
-                <div class="row">
-                    <div class="col-3">
-                        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-                    </div>
-                    <div class="col-9">
-                        <div class="row me-5">
-                            <div class="col-10">yayaya</div>
-                            <hr>
-                            <div class="col-10">yayaya</div>
-                            <hr>
-                            <div class="col-10">yayaya</div>
-                            <hr>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- endforech -->
 </main>
 
 
