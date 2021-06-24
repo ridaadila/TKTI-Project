4b6cac2357e008634d906c187c3af54590dd730e
vaniameith <vameitaqiyyah@gmail.com>
2021-06-24 14:49:12 +0700

    front end ++

--- src/resources/views/ITProcess/business.blade.php
+++ src/resources/views/ITProcess/business.blade.php
@@ -17,15 +17,16 @@
                 <div class="row">
                     <div class="col-3">
                         {{-- <input type="hidden" name="id_perspective" value="{{$item->ID_PERSPECTIVE}}"> --}}
-                        <h1 class="display-5 fw-bold"> {{$item->PERSPECTIVE}} </h1>
+                        <h1 class="display-5 fw-bold" style="justify-content: center; display: flex; align-items: center;"> {{$item->PERSPECTIVE}} </h1>
                     </div>
                     <div class="col-9">
                         <div class="row me-5">
                             @foreach($item->business as $dt)
                             <div class="col-10">
-                                <a href="{{url('IT_goals/' . $item->ID_PERSPECTIVE . '/' . $dt->ID_BUSINESS)}}">
+                                <a class="text-decoration-none" style="color:white; text-decoration: none;" href="{{url('IT_goals/' . $item->ID_PERSPECTIVE . '/' . $dt->ID_BUSINESS)}}">
                                     {{-- <input type="hidden" name="id_business" value="{{$dt->ID_BUSINESS}}"> --}}
                                     {{$dt->DESC_BUSINESS}}
+                                    <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
                                 </a>
                             </div>
                             <hr>
